d61dada2822bc9ab8026ddb93ee44336799b3222
Murillo de Jesus <murillodejesus02@gmail.com>
2026-05-19 19:56:05 -0300

    utilizando o format

--- variaveis.py
+++ variaveis.py
@@ -4,4 +4,4 @@ lucro = faturamento - custo
 
 #print('O faturamento da Loja foi ' + str(faturamento) + ' .O Custo da Loja foi ' + str(custo) + 'O Lucro da Loja foi ' + str(lucro))
 
-print('O faturamento da loja foi {}' .format(faturamento))
+print('O faturamento da loja foi {}. O Custo da Loja foi {}. O Lucro da Loja foi {}' .format(faturamento, custo, lucro))
