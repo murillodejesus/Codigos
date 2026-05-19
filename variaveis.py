cfc3a0fad0b2ee08e4baf65d3f4f9547ad8d8ff9
Murillo de Jesus <murillodejesus02@gmail.com>
2026-05-19 19:51:27 -0300

    testes

--- variaveis.py
+++ variaveis.py
@@ -2,6 +2,4 @@ faturamento = 1000
 custo = 250
 lucro = faturamento - custo
 
-print('O faturamento da Loja foi ' + str(faturamento))
-print('O Custo da Loja foi ' + str(custo))
-print('O Lucro da Loja foi ' + str(lucro))
+print('O faturamento da Loja foi ' + str(faturamento) + ' .O Custo da Loja foi ' + str(custo) + 'O Lucro da Loja foi ' + str(lucro))
