15681a9b19fc58943f13e9bac7b6fc6b3bf3c2ea
Murillo de Jesus <murillodejesus02@gmail.com>
2026-05-19 19:50:01 -0300

    teste de variáveis

--- variaveis.py
+++ variaveis.py
@@ -1,4 +1,7 @@
 faturamento = 1000
-custo = 500
+custo = 250
+lucro = faturamento - custo
 
 print('O faturamento da Loja foi ' + str(faturamento))
+print('O Custo da Loja foi ' + str(custo))
+print('O Lucro da Loja foi ' + str(lucro))
