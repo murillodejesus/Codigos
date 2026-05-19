5bd851819168a3e2800617e068c59b5e138bf4a1
Murillo de Jesus <murillodejesus02@gmail.com>
2026-05-19 20:15:37 -0300

    variaveis e novos testes

--- variaveis.py
+++ variaveis.py
@@ -1,6 +1,6 @@
-faturamento = 1000
-custo = 250
-lucro = faturamento - custo
+# faturamento = 1000
+# custo = 250
+# lucro = faturamento - custo
 
 #print('O faturamento da Loja foi ' + str(faturamento) + ' .O Custo da Loja foi ' + str(custo) + 'O Lucro da Loja foi ' + str(lucro))
 
@@ -8,4 +8,14 @@ lucro = faturamento - custo
 #print('O faturamento da loja foi {}. O Custo da Loja foi {}. O Lucro da Loja foi {}' .format(faturamento, custo, lucro))
 
 # com f-string 
-print(f"O Faturamento foi de {faturamento} e o Lucro foi {lucro}")
+# print(f"O Faturamento foi de {faturamento} e o Lucro foi {lucro}")
+
+faturamento = input("Insira o Faturamento: ")
+custo = input("Insira o custo: ") 
+
+print(type(faturamento))
+print(type(custo))
+
+lucro = faturamento - custo
+
+print(lucro)
